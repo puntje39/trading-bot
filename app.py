ca789cb93b082559a9b9986d323c1972ed49586e
puntje39 <puntgjenj@gmail.com>
2026-04-12 20:54:08 +0200

    Refactor app.py for better readability

--- app.py
+++ app.py
@@ -29,5 +29,5 @@ import os
 if __name__ == "__main__":
     
     threading.Thread(target=run_bot).start()
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT",10000))
     app.run(host="0.0.0.0", port=port)
