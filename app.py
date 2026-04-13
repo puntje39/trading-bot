ade055a8cc9a6a57bf5bf0585053f7ff3e2fb32c
puntje39 <puntgjenj@gmail.com>
2026-04-13 21:04:49 +0200

    Reformat app.py with consistent import order

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import threading
 import time
 import random
 import requests
+import os
 app = Flask(__name__)
 CORS(app)
 
@@ -65,5 +66,6 @@ import os
 if __name__ == "__main__":
     import threading
     threading.Thread(target=run_bot).start()
+    
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
